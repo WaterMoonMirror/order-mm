816f951fa5e48c280227a3e9a0f2a0c67de3c572
tkn <lizhu0227@163.com>
2018-07-05 23:41:59 +0800

    完成创建订单、订单入库

---
+++ src/main/java/com/example/order/service/OrderService.java
@@ -0,0 +1,13 @@
+package com.example.order.service;
+
+import com.example.order.dtd.OrderDTO;
+
+public interface OrderService {
+
+    /**
+     * 创建订单
+     * @param orderDTO
+     * @return
+     */
+    OrderDTO create(OrderDTO orderDTO);
+}
